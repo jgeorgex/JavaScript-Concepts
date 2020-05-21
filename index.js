b2105e5dc96c91666e82f723cadc53b91f3e259b
Julian George <jules104@gmail.com>
2020-05-21 12:16:27 +0100

    Revise scope chain explanation and add IIFE's with Kursty the clown, Sideshow Bob and Sideshow Mel

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//JavaScript Concepts
+
+// JavaScript Scope Chain & Execution Context with Homer, Bart and Maggie.
+
 function homer() {
   console.log(maggie);
 }
@@ -10,10 +14,37 @@ function bart() {
 var maggie = 1;
 bart();
 
-// Line 11 says run bart.
-// The bart function is on line 5 and runs.
+// Explanation
+
+// Line 15 says run bart.
+// The bart function is on line 9 and runs.
 // The bart function creates a variable local to it called maggie and asigns maggie a value of 2.
-// The bart function then runs the homer function which is on line 1.
+// The bart function then runs the homer function which is on line 5.
 // The homer function logs to the console maggie. which is a value of 1.
-// homer console log's 1 because the variable maggie with teh value of 1 is set in the global context.
-// The variable of 1 for maggie set in bart is local to bart so homer doesnt know about that even though homer was called in to action by bart.
+// Why 1 and not 2 or undefined?
+// homer console log's 1 because the variable maggie with the value of 1 is set in the global scope.
+// maggie with the value of 2 only exists in the bart function context.  It doesnt exist outside of that.
+// So when the bart function runs the homer function the homer function does not know about maggie with the value of 2.
+// When homer looks for maggie (line 6) Therses no maggie locally (in the homer function).
+// What JavaScript does is it then goes off to look for maggie exlsewhere in the execution stack.
+// It then finds maggie on line 14 as maggie on line 14 does not exist locally maggie is in the global scope.
+
+// Imediately Invoked Function Expressions (IIFEs)
+
+// function statement
+function greet(name) {
+  console.log('Hello ' + name);
+}
+greet('Sideshow Bob');
+
+// function expression
+var greetFunc = function(name) {
+  console.log('Hello ' + name);
+};
+greetFunc('Sideshow Mel');
+
+// IIFE
+var greeting = (function(name) {
+  console.log('Hello ' + name);
+})();
+console.log(greeting);
